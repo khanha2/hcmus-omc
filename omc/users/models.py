8ff56a02e0793f6bf36325e3c5de66f944a4b53b
root <root@test.virtualizor.com>
2017-03-03 23:28:11 +0000

    Hot fix: Fix unicode error when showing on admin site

--- omc/users/models.py
+++ omc/users/models.py
@@ -47,5 +47,5 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __unicode__(self):
         if not self.first_name or not self.last_name:
-            return '%' % (self.username)
+            return '%' % (unicode(self.username, 'utf-8'))
         return '%s %s' % (self.last_name, self.first_name)
